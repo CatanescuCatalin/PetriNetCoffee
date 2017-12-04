020a86832ca20f39df9cff3d20eed56cf8b33582
MacBook <MacBook@192.168.1.4>
2017-12-04 21:46:08 +0200

    Initial Commit

---
+++ PetriNetCoffee/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PetriNetCoffee
+//
+//  Created by MacBook on 04/12/2017.
+//  Copyright © 2017 MacBook. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
